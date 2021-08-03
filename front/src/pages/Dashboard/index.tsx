14ed9cab35ecb2b8500eb2abb664a10266a32e91
Renan de Freitas <rnn.frts@gmail.com>
2021-08-02 23:12:54 -0300

    created export data button for users, abrigos and cursos

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -5,20 +5,20 @@ import Navbar from '../../components/Navbar';
 import NavbarDesktop from '../../components/NavbarDesktop';
 import TopMenu from '../../components/TopMenu';
 
-import { Container, NotificationContainer, DashboardContent, User, AbrigosList, Abrigo } from './styles';
+import { Container, NotificationContainer, DashboardContent, User, AbrigosList, Abrigo, AdminNotices } from './styles';
 import { Link, Redirect } from 'react-router-dom';
 
 import Perfil from '../../images/perfil-avatar.png'
 import { FiMinusCircle } from 'react-icons/fi';
 
-import { useQuery } from '@apollo/client';
-import { GET_ABRIGOS, GET_USERS, VIEW_NOTIFICATIONS } from './apolloQueries';
+import { useLazyQuery, useQuery } from '@apollo/client';
+import { GET_ABRIGOS, GET_USERS, VIEW_NOTIFICATIONS, DOWNLOAD_CURSOS, DOWNLOAD_ABRIGOS, DOWNLOAD_USUARIOS } from './apolloQueries';
 
 interface IProfissionaisData {
   id: string;
   nome: string;
   profissao: string;
-}
+} 
 
 interface IProfissionaisQuery {
   verUsuarios: IProfissionaisData[];
@@ -41,12 +41,19 @@ interface INotification {
   }[]
 }
 
+interface IExportAbrigos {  exportarAbrigos: string; }
+interface IExportCursos {  exportarCursos: string; }
+interface IExportUsuarios {  exportarUsuarios: string; }
+
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
 
   const { data: profissionaisQl } = useQuery<IProfissionaisQuery>(GET_USERS);
   const { data: abrigosQl } = useQuery<IAbrigosQuery>(GET_ABRIGOS);
   const { data: notificationsQl } = useQuery<INotification>(VIEW_NOTIFICATIONS);
+  const [ getAbrigosCSV, { data: abrigosCSVurl }] = useLazyQuery<IExportAbrigos>(DOWNLOAD_ABRIGOS);
+  const [ getCursosCSV, { data: cursosCSVurl }] = useLazyQuery<IExportCursos>(DOWNLOAD_CURSOS);
+  const [ getUsuariosCSV, { data: usuariosCSVurl }] = useLazyQuery<IExportUsuarios>(DOWNLOAD_USUARIOS);
 
   return (
     <Container>
@@ -57,6 +64,23 @@ const Dashboard: React.FC = () => {
       {user.credencial === "Aluno" && <Redirect to="/Assistir" />}
 
       <DashboardContent>
+        {user.credencial === "Admin" && (
+          <>
+            <h2>download de arquivos</h2>
+            <AdminNotices>
+              <p>As planilhas abaixo contém um compilado de todos os abrigos, usuários e cursos.</p>
+              <div>
+                <button type="button" onClick={() => getAbrigosCSV()}>exportar abrigos</button>
+                <button type="button" onClick={() => getCursosCSV()}>exportar cursos</button>
+                <button type="button" onClick={() => getUsuariosCSV()}>exportar usuários</button>
+              </div>
+              {abrigosCSVurl && <Redirect to={abrigosCSVurl.exportarAbrigos} />}
+              {cursosCSVurl && <Redirect to={cursosCSVurl.exportarCursos} />}
+              {usuariosCSVurl && <Redirect to={usuariosCSVurl.exportarUsuarios} />}
+            </AdminNotices>
+          </>
+        )}
+        
         {notificationsQl && notificationsQl.verNotificacoes.length > 0 && <h2>notificações recentes</h2>}
         {notificationsQl && notificationsQl.verNotificacoes.map(notification => (
           <NotificationContainer key={notification.id} type={notification.tipo}>
